23e93334c704403eb96ae34cef1b344736f463d6
David Potts <pottsdavidg@gmail.com>
2021-07-20 17:00:34 -0400

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,258 @@
+const nudesAndFigures = [
+    {
+        title: 'Cherry Cordial 1', 
+        style: 'Acrylic on Panel', 
+        size: '20in. x 24in.', 
+        year: 2018,
+        link: '../images/bodies-of-work/cherry 1.jpg'
+    },
+    {
+        title: 'Cherry Cordial 2',
+        style: 'Acrylic on Panel',
+        year: 2018,
+        link: '../images/bodies-of-work/Cherry 2 Bigger.jpg'
+    },
+    {
+        title: 'Lotus Object',
+        style: 'Acrylic on Panel',
+        size: '12in. x 24in.',
+        year: 2019,
+        link: '../images/bodies-of-work/Maureen_OKeefe_Lotus Object.jpg'
+    },
+    {
+        title: 'Lotus Object II',
+        style: 'Acrylic on Panel',
+        size: '24in. x 48in.',
+        year: 2019,
+        link: '../images/bodies-of-work/OKeefe_Maureen_LotusObject2_acrylic_24x48inch.jpg'
+    },
+    {
+        title: 'Idolatry',
+        style: 'Acrylic on Panel',
+        size: '24in. x 48in.',
+        year: 2019,
+        link: '../images/bodies-of-work/OKeefe_Maureen_Idolatry_acrylic_24x48inch.jpg'
+    },
+    {
+        title: 'Objectify',
+        style: 'Acrylic on Panel',
+        size: '24in. x 48in.',
+        year: 2019,
+        link: '../images/bodies-of-work/OKeefe_Maureen_Objectify_acrylic_24x48inch.jpg'
+    },
+    {
+        title: 'Shrimp and Grits',
+        style: 'Acrylic on Panel',
+        size: '24in. x 36in.',
+        year: 2017,
+        link: '../images/bodies-of-work/OKeefe_Maureen_Shrimp and Grits_acrylic_24x36in.jpg'
+    },
+    {
+        title: 'Reclining Study',
+        style: 'Acrylic on Paper',
+        size: '6in. x 8in.',
+        year: 2019,
+        link: '../images/bodies-of-work/Reclining Nude Study.jpg'
+    },
+    {
+        title: 'Housewives Love Kirby',
+        style: 'Acrylic on Panel',
+        size: '36in. x 48in.',
+        year: 2018,
+        link: '../images/mirrored-works/HousewivesLoveKirby_acrylic_36x48_1200.jpg'
+    },
+    {
+        title: 'Parallax Eve 2',
+        link: '../images/bodies-of-work/Parallax Eve2.jpg'
+    },
+    {
+        title: 'Solar Maiden',
+        link: '../images/bodies-of-work/Solar Maiden.jpg'
+    }
+];
+
+const mirroredWorks = [
+    {
+        title: 'Annointing of the Sick',
+        style: 'Acrylic on Panel',
+        size: '48in. x 48in.',
+        year: 2021,
+        link: '../images/mirrored-works/Annointing of the Sick.jpg'
+    },
+    {
+        title: 'Baptized',
+        style: 'Acrylic on Panel',
+        size: '48in. x 48in.',
+        year: 2021,
+        link: '../images/mirrored-works/Baptized.jpg'
+    },
+    {
+        title: 'Cusp',
+        style: 'Acrylic on Panel',
+        size: '48in. x 48in.',
+        year: 2020,
+        link : '../images/mirrored-works/Maureen_OKeefe_Cusp.jpg'
+    },
+    {
+        title: 'Forty Is The New Thirty',
+        style: 'Acrylic on Panel',
+        size: '48in. x 48in.',
+        year: 2020,
+        link: '../images/mirrored-works/Maureen_OKeefe_FortyIsTheNewThirty_acrylic_36x36_1000.jpg'
+    },
+    {
+        title: 'Music for a Funeral',
+        style: 'Acrylic on Panel',
+        size: '30in. x 30in.',
+        year: 2021,
+        link: '../images/mirrored-works/Maureen_OKeefe_MusicForAFuneral_acrylic_30x30_1000.jpg'
+    },
+    {
+        title: 'Real Power',
+        style: 'Acrylic on Panel',
+        size: '24in. x 24in.',
+        year: 2020,
+        link: '../images/mirrored-works/Maureen_OKeefe_Real Power.jpg',
+        sold: true
+    },
+    {
+        title: 'Penance',
+        link: '../images/mirrored-works/Penance.jpg'
+    }
+];
+
+const trinitiesAndSpirits = [
+    {
+        title: 'American Spirit',
+        style: 'Acrylic on Panel',
+        year: 2016,
+        link: '../images/trinities-and-spirits/American Spirit.jpg',
+        sold: true
+    },
+    {
+        title: 'American Spirit II',
+        style: 'Acrylic on Panel',
+        year: 2016,
+        link: '../images/trinities-and-spirits/AmSpIIBig.jpg',
+        sold: true
+    },
+    {
+        title: 'Matilda Joslyn Gage',
+        style: 'Acrylic on Panel',
+        year: 2016,
+        link: '../images/trinities-and-spirits/V_Matilda Jocyln Gage.jpg'
+    },
+    {
+        title: 'Bleeding Heart',
+        style: 'Acrylic on Panel',
+        year: 2019,
+        link: '../images/trinities-and-spirits/BVT05086.jpg'
+    },
+    {
+        title: 'Allegory',
+        style: 'Acrylic on Panel',
+        year: 2020,
+        link: '../images/trinities-and-spirits/BVT05083.jpg'
+    },
+    {
+        title: 'Pocket Full of Posies',
+        style: 'Acrylic on Panel',
+        year: 2020,
+        link: '../images/trinities-and-spirits/BVT05082.jpg',
+        sold: true
+    },
+    {
+        title: 'Still Life',
+        style: 'Acrylic on Panel',
+        year: 2018,
+        link: '../images/trinities-and-spirits/OKeefe_Maureen_Still Life.jpg'
+    },
+    {
+        title: 'Milk Spirit',
+        style: 'Acrylic on Panel',
+        year: 2017,
+        link: '../images/trinities-and-spirits/V_Milk Spirit.jpg',
+        sold: true
+    },
+    {
+        title: 'Harvest Spirit',
+        style: 'Acrylic on Panel',
+        year: 2018,
+        link: '../images/trinities-and-spirits/V_Harvest Spirit.jpg',
+        sold: true
+    },
+    {
+        title: 'Renaissance Spirit',
+        style: 'Acrylic on Panel',
+        year: 2018,
+        link: '../images/trinities-and-spirits/RenaissanceSpiritBig.jpg',
+        sold: true
+    },
+    {
+        title: 'UnAmerican',
+        style: 'Acrylic on Panel',
+        link: '../images/trinities-and-spirits/UnAmerican Big.jpg'
+    }
+];
+
+const blindContourPortraits = [];
+
+const stillLife = [
+    {
+        title: 'Cycles Perfecta',
+        style: 'Acrylic on Paper',
+        year: 2020,
+        link: '../images/still-life/Cycles Perfecta.jpg',
+        sold: true
+    },
+    {
+        title: 'Fluarantine',
+        style: 'Acrylic on Panel',
+        year: 2020,
+        link: '../images/still-life/Fluarantine.jpg'
+    },
+    {
+        title: 'Lilies BC',
+        style: 'Acrylic on Panel',
+        year: 2020,
+        link: '../images/still-life/Lilies BC.jpg',
+        sold: true
+    },
+    {
+        title: 'Magnolia BC',
+        style: 'Acrylic on Panel',
+        year: 2020,
+        link: '../images/still-life/Magnolia BC.jpg',
+        sold: true
+    },
+    {
+        title: 'This is America',
+        style: 'Acrylic on Panel',
+        year: 2020,
+        link: '../images/still-life/Skull and FlowerBC.jpg'
+    },
+    {
+        title: 'Original I',
+        link: '../images/still-life/OriginalI.jpg'
+    },
+    {
+        title: 'Original II',
+        link: '../images/still-life/OriginalII.jpg'
+    }
+
+];
+
+const idolatry = [];
+
+/*
+export {
+    nudesAndFigures, 
+    mirroredWorks, 
+    trinitiesAndSpirits, 
+    blindContourPortraits, 
+    stillLife, 
+    idolatry
+};
+*/
+
+console.log(nudesAndFigures.filter(item => item.title === 'Lotus Object')[0]);
---
+++ scripts/work.js
@@ -0,0 +1,126 @@
+import * as art from './app.js';
+
+const all = [
+    ...art.nudesAndFigures, 
+    ...art.mirroredWorks,
+    ...art.trinitiesAndSpirits,
+    ...art.stillLife
+];
+
+const title = [
+    'home-page', 
+    'mirw-page', 
+    'nude-page', 
+    'trin-page', 
+    'blin-page', 
+    'stil-page', 
+    'idol-page'
+];
+
+const returnTemplate = (link) => {
+    return `
+    <div class="image-container" onclick="setHiddenDisplay('${link}')">
+        <a href="#large-display">
+            <img class="list-image" src="${link}" width="auto">
+        </a>
+    </div>
+    `;
+}
+
+const fetchArtTemplate = (key) => {
+
+    let result = '';
+
+    if (key == 'nude' || key == 'all') {
+        art.nudesAndFigures.forEach(item => {
+            result += returnTemplate(item.link);
+        });
+    }
+
+    if (key == 'mirw' || key == 'all') {
+        art.mirroredWorks.forEach(item => {
+            result += returnTemplate(item.link);
+        });
+    }
+
+    if (key == 'trin' || key == 'all') {
+        art.trinitiesAndSpirits.forEach(item => {
+            result += returnTemplate(item.link);
+        });
+    }
+
+    if (key == 'stil' || key == 'all') {
+        art.stillLife.forEach(item => {
+            result += returnTemplate(item.link);
+        });
+    }
+
+    return result;
+
+}
+
+const setHiddenDisplay = (aLink) => {
+
+    let result = '';
+    const displayImage = all.filter(image => image.link === aLink)[0];
+    
+    result += `<img class="large-image" src="${displayImage.link}">`;
+    result += `<div class="large-image-description">`;
+    result += `<div>${displayImage.title}</div>`;
+
+    if (displayImage.hasOwnProperty('style')) result += `<div>${displayImage.style}</div>`;
+    if (displayImage.hasOwnProperty('size')) result += `<div>${displayImage.size}</div>`;
+    if (displayImage.hasOwnProperty('year')) result += `<div>${displayImage.year}</div>`;
+    if (displayImage.hasOwnProperty('sold')) result += `<div class="error-text">SOLD</div>`;
+
+    result += `</div><a href="#" id="cancel">BACK</a>`;
+    document.getElementById('large-display').innerHTML = result;
+
+}
+
+const updateDisplay = (page) => {
+
+    const content = '';
+    const display = document.getElementById('display');
+
+    if (page == 'home-page') content += fetchArtTemplate('all');
+    if (page == 'stil-page') content += fetchArtTemplate('stil');
+    if (page == 'mirw-page') content += fetchArtTemplate('mirw');
+    if (page == 'nude-page') content += fetchArtTemplate('nude');
+    if (page == 'trin-page') content += fetchArtTemplate('trin');
+    if (page == 'blin-page') content += fetchArtTemplate('all');
+    if (page == 'idol-page') content += fetchArtTemplate('all');
+
+    display.append(content);
+
+}
+
+const selectPage = (ID) => {
+    
+    let tagless = title.filter(name => name != ID);
+    tagless.forEach(tag => {
+        document.getElementById(tag).classList.remove('selected-link');
+    });
+    document.getElementById(ID).classList.add('selected-link');
+    updatePage();
+
+}
+
+const updatePage = () => {
+
+    let elementClasses = [
+        document.getElementById(title[0]).classList,
+        document.getElementById(title[1]).classList,
+        document.getElementById(title[2]).classList,
+        document.getElementById(title[3]).classList,
+        document.getElementById(title[4]).classList,
+        document.getElementById(title[5]).classList,
+        document.getElementById(title[6]).classList
+    ];
+
+    for (let i = 0; i < 7; i++) {
+        if (elementClasses[i].includes('selected-link')) {
+            updateDisplay(title[i]);
+        }
+    }
+}
